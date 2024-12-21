70446487842345dfeba7b25812a39f6294ab7875
Omair Hamd alla <o2m0a0i3r@gmail.com>
2024-12-22 02:11:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ mp_face_mesh = mp.solutions.face_mesh
 mp_drawing = mp.solutions.drawing_utils
 
 def get_face_landmarks(image):
-    #Settings for face mesh
+    # Settings for the face mesh -- To add more
     with mp_face_mesh.FaceMesh(static_image_mode=False , max_num_faces=1 , min_detection_confidence=0.5) as face_mesh:
         results = face_mesh.process(image)
         return results
@@ -13,13 +13,13 @@ def get_face_landmarks(image):
 
 cap = cv2.VideoCapture(0)
 
-#The running loop
+######### Loop 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
     
-    # To flip image for user
+    # Flipping image just for user /// Not Necessary 
     frame = cv2.flip(frame,1)
     rgb_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     results = get_face_landmarks(rgb_frame)
@@ -27,7 +27,7 @@ while cap.isOpened():
     if results.multi_face_landmarks:
         for face_landmarks in results.multi_face_landmarks: 
 
-            # Drawing the face mesh
+            # Drawing
             mp_drawing.draw_landmarks(
                 frame,
                 face_landmarks,
@@ -35,7 +35,6 @@ while cap.isOpened():
                 mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=1, circle_radius=1),
                 mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=1, circle_radius=1)
             )
-
 
     cv2.imshow("Face_Mesh_program" , frame)
     if cv2.waitKey(5) & 0xFF == 27 or cv2.waitKey(5) & 0xFF == ord('q') :
